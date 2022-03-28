3c6c964ecb4821c76821c3194ba3d089666f1066
polatine <101523958+polatine@users.noreply.github.com>
2022-03-28 20:17:09 +0200

    Improved contract, no dependency to erc721uristorage! cheaper!

--- polatine.sol
+++ polatine.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Polatine is ERC721URIStorage {
+contract Polatine is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -15,17 +13,17 @@ contract Polatine is ERC721URIStorage {
     mapping(address => string) private _subcollectionName; //Which subcollection a address represents
     mapping(address => uint256) private _deliverability; //Level of deliverability a subcollection has 
     mapping(address => string) private _tokenUri; //The Uri for a whole subcollection
-    mapping(address => uint256) private _price; //The amount the artist gets as payment
+    mapping(address => uint256) private _price; //The amount the artist gets as payment in WEI
     mapping(uint256 => address) private _tokenSubcollection; //Mapping from token to which subcollection it belongs to
 
     constructor() ERC721("Polatine", "PLT") {}
 
-    function register(string memory name, uint256 mintsLeft, uint256 deliverability, string memory tokenURI, uint256 price) public {
+    function register(string memory name, uint256 mintsLeft, uint256 deliverability, string memory newTokenURI, uint256 price) public {
         require(keccak256(bytes(_subcollectionName[_msgSender()])) == keccak256(bytes("")), "Polatine: address already registered to a subcollection.");
         _subcollectionName[_msgSender()] = name;
         _mintsLeft[_msgSender()] = mintsLeft;
         _deliverability[_msgSender()] = deliverability;
-        _tokenUri[_msgSender()] = tokenURI;
+        _tokenUri[_msgSender()] = newTokenURI;
         _price[_msgSender()] = price;
     }
 
@@ -44,6 +42,9 @@ contract Polatine is ERC721URIStorage {
     function priceOf(address subcollectionAddress) public view virtual returns (uint256) {
         return _price[subcollectionAddress];
     }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return _tokenUri[_tokenSubcollection[tokenId]];
+    }
 
     function claimNFT(address subcollectionAddress)
         public payable
@@ -54,11 +55,11 @@ contract Polatine is ERC721URIStorage {
         require(msg.value == _price[subcollectionAddress], "Polatine: deposit does not match subcollection price."); // msg.value is how much ether was sent
         
         payable(subcollectionAddress).transfer(msg.value);// send the ether to subcollection owner
-        _tokenIds.increment();
         
+        _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(_msgSender(), newItemId);
-        _setTokenURI(newItemId, _tokenUri[subcollectionAddress]);
+
         _mintsLeft[subcollectionAddress] -= 1;
         _tokenSubcollection[newItemId] = subcollectionAddress;
 
