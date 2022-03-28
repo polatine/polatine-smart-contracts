16e4c861393c8ae6f54c447c59bd712fc52d238c
polatine <101523958+polatine@users.noreply.github.com>
2022-03-28 14:06:47 +0200

    claim function implemented, payment enabled and bugs fixed

--- polatine.sol
+++ polatine.sol
@@ -11,59 +11,56 @@ contract Polatine is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(address => uint256) private _mintsLeft;
-    mapping(address => string) private _artistName;
-    mapping(address => uint256) private _deliverability;
-    mapping(address => string) private _tokenUri;
-    mapping(address => uint256) private _price;
-
+    mapping(address => uint256) private _mintsLeft; //How many mintings a subcollection has left
+    mapping(address => string) private _subcollectionName; //Which subcollection a address represents
+    mapping(address => uint256) private _deliverability; //Level of deliverability a subcollection has 
+    mapping(address => string) private _tokenUri; //The Uri for a whole subcollection
+    mapping(address => uint256) private _price; //The amount the artist gets as payment
+    mapping(uint256 => address) private _tokenSubcollection; //Mapping from token to which subcollection it belongs to
 
     constructor() ERC721("Polatine", "PLT") {}
 
     function register(string memory name, uint256 mintsLeft, uint256 deliverability, string memory tokenURI, uint256 price) public {
-        require(_artistName[_msgSender()] == "");
-        _artistName[_msgSender()] = name;
+        require(keccak256(bytes(_subcollectionName[_msgSender()])) == keccak256(bytes("")), "Polatine: address already registered to a subcollection.");
+        _subcollectionName[_msgSender()] = name;
         _mintsLeft[_msgSender()] = mintsLeft;
         _deliverability[_msgSender()] = deliverability;
         _tokenUri[_msgSender()] = tokenURI;
         _price[_msgSender()] = price;
     }
 
-    function artistNameOf(address artistAddress) public view virtual returns (string memory) {
-        return _artistName[artistAddress];
+    function subcollectionNameOf(address subcollectionAddress) public view virtual returns (string memory) {
+        return _subcollectionName[subcollectionAddress];
     }
-    function mintsLeftOf(address artistAddress) public view virtual returns (uint256) {
-        return _mintsLeft[artistAddress];
+    function mintsLeftOf(address subcollectionAddress) public view virtual returns (uint256) {
+        return _mintsLeft[subcollectionAddress];
     }
-    function deliverabilityOf(address artistAddress) public view virtual returns (uint256) {
-        return _deliverability[artistAddress];
+    function deliverabilityOf(address subcollectionAddress) public view virtual returns (uint256) {
+        return _deliverability[subcollectionAddress];
     }
-
-    function _mint(address to, uint256 tokenId) internal virtual {
-        require(to != address(0), "ERC721: mint to the zero address");
-        require(!_exists(tokenId), "ERC721: token already minted");
-
-        _beforeTokenTransfer(address(0), to, tokenId);
-
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(address(0), to, tokenId);
-
-        _afterTokenTransfer(address(0), to, tokenId);
+    function tokenSubcollection(uint256 tokenId) public view virtual returns (address) {
+        return _tokenSubcollection[tokenId];
+    }
+    function priceOf(address subcollectionAddress) public view virtual returns (uint256) {
+        return _price[subcollectionAddress];
     }
 
-    function mintNFT(address artistAddress, uint256 mintsLeft)
-        public
+    function claimNFT(address subcollectionAddress)
+        public payable
         returns (uint256)
     {
-        require(_mintsLeft[_msgSender()]>0);
+        require(keccak256(bytes(_subcollectionName[subcollectionAddress])) != keccak256(bytes("")), "Polatine: address does not represent a subcollection.");
+        require(_mintsLeft[subcollectionAddress]>0, "Polatine: subcollection has no more mints left.");
+        require(msg.value == _price[subcollectionAddress], "Polatine: deposit does not match subcollection price."); // msg.value is how much ether was sent
+        
+        payable(subcollectionAddress).transfer(msg.value);// send the ether to subcollection owner
         _tokenIds.increment();
         
         uint256 newItemId = _tokenIds.current();
         _mint(_msgSender(), newItemId);
-        _setTokenURI(newItemId, _tokenUri[_msgSender()]);
-        _mintsLeft[_msgSender()] -= 1;
+        _setTokenURI(newItemId, _tokenUri[subcollectionAddress]);
+        _mintsLeft[subcollectionAddress] -= 1;
+        _tokenSubcollection[newItemId] = subcollectionAddress;
 
         return newItemId;
     }
